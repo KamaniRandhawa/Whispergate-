43fbefb19d7431662c3a1484b4028e695c2ff2e1
KamaniRandhawa <kaminikaur10@gmail.com>
2025-04-19 15:25:42 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("WHISPER GATE ACTIVATED");
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'm' || e.key === 'g' || e.key === 'x') {
+      console.log("THEY ARE WATCHING");
+    }
+  });
